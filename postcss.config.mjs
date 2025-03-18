3aba54c4ed8be56dfaca7beb680d279c9f183128
Ricardo Do Vale <pataco80@gmail.com>
2025-03-18 14:37:26 +0100

    chore: update project configuration files
    Auto stash before rebase of "develop"

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,5 +1,13 @@
+import tailwindcss from 'tailwindcss'
+import autoprefixer from 'autoprefixer'
+import animate from 'tailwindcss-animate'
+
 const config = {
-	plugins: ['@tailwindcss/postcss'],
+	plugins: {
+		tailwindcss,
+		autoprefixer,
+		'tailwindcss-animate': animate,
+	},
 }
 
 export default config
